4f2f6603fd5f20015c9bdfc251977ffddcdd19c0
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2022-04-03 16:07:26 +0530

    Create constants.js

--- scrapers/constants.js
+++ scrapers/constants.js
@@ -1,11 +1,11 @@
 module.exports = {
-    RARGB_BASE_URL: "https://rarbgp2p.org/torrents.php?search=",
+    RARGB_BASE_URL: "https://rargb.to/search/?search=",
     KICKASS_BASE_URL: "https://kickasstorrents.to/usearch/",
     SKYTORRENTS_BASE_URL: "https://www.skytorrents.lol/?query=",
     BASE_URL_1337X: "https://proxy.goincop1.workers.dev/https/www.1337x.to/search/",
     LIMETORRENTS_BASE_URL : "https://proxy.goincop1.workers.dev/https/www.limetorrents.pro/search/all/",
     TORRENTGALAXY_BASE_URL : "https://torrentgalaxy.tv/torrents.php?search=",
-    TORRENTDOWNLOADS_BASE_URL : "https://www.torrentdownload.info/search?q=",
+    TORRENTDOWNLOADS_BASE_URL : "https://proxy.goincop1.workers.dev/https/www.torrentdownload.info/search?q=",
     NYAA_BASE_URL : "https://nyaa.si/?q=",
     THEPIRATEBAY_BASE_URL : "https://proxypirate.gq/s/?q=",
     HORRIBLESUBS_BASE_URL : "https://nyaa.si/user/HorribleSubs?f=0&c=0_0&q="
